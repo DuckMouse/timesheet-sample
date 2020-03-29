e1e606b0e68d0a6d1593fca3cd7dfd203a237e7a
auret_swanepoel <auretswanepoel@rocketmail.com>
2020-03-30 10:20:07 +1100

    clean: cleaned main timesheet

--- libs/timesheet/ui/src/lib/timesheet-table/timesheet-table.component.ts
+++ libs/timesheet/ui/src/lib/timesheet-table/timesheet-table.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 
 import { GridApi, RowNode } from 'ag-grid-community';
-import { format } from 'date-fns';
 import { uuid } from 'uuidv4';
 
 import {
@@ -14,100 +13,23 @@ import {
 import { calculateTotal } from '@timesheet/utilities';
 import { environment } from '@env/environment';
 
-import { DurationEditorComponent } from '../duration-editor/duration-editor.component';
-import { TimeDurationComponent } from '../time-duration/time-duration.component';
-import { EntryActionActiveComponent } from './../entry-action-active/entry-action-active.component';
-import { EntryActionNewComponent } from './../entry-action-new/entry-action-new.component';
 import { YesnomodalComponent } from '../yesnomodal/yesnomodal.component';
+import { gridOptions } from './grid-options';
 
 @Component({
   selector: 'timesheet-timesheet-table',
   templateUrl: './timesheet-table.component.html',
   styleUrls: ['./timesheet-table.component.scss']
 })
-export class TimesheetTableComponent implements OnInit {
+export class TimesheetTableComponent {
   @Input() timesheetEntries: TimesheetEntry[] = [];
   @Output() submitSelectedActiveEntries = new EventEmitter<TimesheetEntry[]>();
 
-  private gridApi: GridApi;
   context: any;
-  frameworkComponents = {
-    timeDurationComponent: TimeDurationComponent,
-    durationEditorComponent: DurationEditorComponent,
-    entryActionNewComponent: EntryActionNewComponent,
-    entryActionActiveComponent: EntryActionActiveComponent
-  };
+  gridOptions = gridOptions;
+  defaultHourlyRate = environment.defaultHourlyRate;
 
-  defaultColDef = {
-    editable: true
-  };
-
-  columnDefs = [
-    {
-      headerName: 'State',
-      field: 'state',
-      editable: false,
-      checkboxSelection: (params: any) =>
-        params.data.state === EntryState.active ? true : false
-    },
-    {
-      headerName: 'Date',
-      field: 'date',
-      cellRenderer: (params: any) =>
-        format(new Date(params.value), 'dd/LL/yyyy'),
-      editable: false
-    },
-    { headerName: 'Title', field: 'title' },
-    {
-      headerName: 'Type',
-      field: 'type',
-      cellEditor: 'agSelectCellEditor',
-      cellEditorParams: {
-        values: [
-          EntryType.draftingDocument,
-          EntryType.research,
-          EntryType.telephoneCall
-        ]
-      }
-    },
-    {
-      headerName: 'Duration (hh:mm)',
-      cellRenderer: 'timeDurationComponent',
-      cellEditor: 'durationEditorComponent',
-      field: 'duration'
-    },
-    {
-      headerName: 'Hourly Rate',
-      field: 'hourlyRate'
-    },
-    {
-      headerName: 'Total',
-      colId: 'total',
-      valueGetter: (params: any) =>
-        calculateTotal(params.data.duration, params.data.hourlyRate),
-      valueFormatter: (params: any) => '$ ' + params.value.toFixed(2),
-      editable: false
-    },
-    ,
-    {
-      headerName: 'Actions',
-      field: 'actions',
-      editable: false,
-      cellRendererSelector: (params: any) => {
-        switch (params.data.state) {
-          case EntryState.active: {
-            return { component: 'entryActionActiveComponent' };
-          }
-          case EntryState.new: {
-            return { component: 'entryActionNewComponent' };
-          }
-          default:
-            return '';
-        }
-      }
-    }
-  ];
-  getRowNodeId = (data: any) => data.id;
+  private gridApi: GridApi;
 
   constructor(private dialog: MatDialog) {
     this.context = {
@@ -115,13 +37,11 @@ export class TimesheetTableComponent implements OnInit {
     };
   }
 
-  ngOnInit(): void {}
-
-  onGridReady(params: any) {
+  onGridReady(params: any): void {
     this.gridApi = params.api;
   }
 
-  executeAction(entryAction: EntryAction, node: RowNode) {
+  executeAction(entryAction: EntryAction, node: RowNode): void {
     switch (entryAction) {
       case EntryAction.save: {
         this.gridApi.updateRowData({
@@ -169,13 +89,15 @@ export class TimesheetTableComponent implements OnInit {
     }
   }
 
-  addNewRow() {
-    const newEntry = this.createNewEmptyRow();
-    this.gridApi.updateRowData({ add: [newEntry], addIndex: 0 });
+  addNewRow(): void {
+    this.gridApi.updateRowData({
+      add: [this.createNewEmptyRow()],
+      addIndex: 0
+    });
     this.gridApi.startEditingCell({ rowIndex: 0, colKey: 'title' });
   }
 
-  submitActiveEntries() {
+  submitActiveEntries(): void {
     const activeEntries = this.gridApi.getSelectedRows();
     if (activeEntries.length > 0) {
       this.submitSelectedActiveEntries.emit(activeEntries);
